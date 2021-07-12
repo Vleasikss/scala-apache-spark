f6baf1b551aa2d856a7b400cb14c9358333bad1e
Vlas <42597604+Vleasikss@users.noreply.github.com>
2021-07-12 15:19:47 +0300

    csv-reading INIT

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,8 @@
 package org.example
 
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 
 object Main {
 
@@ -10,11 +11,30 @@ object Main {
   val CSV_FILE_PATH = "data.csv"
   val JSON_FILE_PATH = "data.json"
 
+
+  // optional (exclude job working)
+  val csvSchema: StructType = StructType(Array(
+    StructField("id", DataTypes.IntegerType),
+    StructField("firstname", DataTypes.StringType),
+    StructField("lastname", DataTypes.StringType),
+    StructField("email", DataTypes.BooleanType),
+    StructField("email2", DataTypes.StringType),
+    StructField("profession", DataTypes.StringType)))
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder
       .master(MASTER)
       .appName(APP_NAME)
       .getOrCreate()
+
+    val df: DataFrame = spark.read
+      .option("delimiter", ",")
+      .option("inferSchema", "true") // automatically recognize data types
+      .schema(csvSchema)
+      .csv(CSV_FILE_PATH)
+
+    df.printSchema()
+    df.show()
 
   }
 }
