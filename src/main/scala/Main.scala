95cb4515e6deb1a10011de7025648a8bd10d8043
Vlas <42597604+Vleasikss@users.noreply.github.com>
2021-07-15 16:51:14 +0300

    pair-rdd INIT

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,13 @@
 package org.example
 
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
+/**
+ * PairRdd<Any,Any>
+ * Key Value
+ */
 object Main {
 
   val MASTER = "local[*]"
@@ -15,6 +20,23 @@ object Main {
       .master(MASTER)
       .appName(APP_NAME)
       .getOrCreate()
+
+    val sc = spark.sparkContext
+
+    val inputData: List[String] = List(
+      "WARN: Tuesday 4 September 0405",
+      "ERROR: Tuesday 4 September 0408",
+      "FATAL: Wednesday 5 September 1632",
+      "ERROR: Friday 7 September 1854",
+      "WARN: Saturday 8 September 1942",
+    )
+
+    val rddLogs: RDD[(String, Long)] = sc.parallelize(inputData)
+      .map(rawValue => (rawValue.split(": ")(0), 1L) )
+      .reduceByKey((value1, value2) => value1 + value2)
+
+    println(rddLogs.foreach(println))
+
 
   }
 }
